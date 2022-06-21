94e5127fa5c814ec0efb1bb71b846035e33ac765
- <nari20030928@gmail.com>
2022-06-21 21:03:28 +0900

    -

--- New_Design/js/style.js
+++ New_Design/js/style.js
@@ -2,19 +2,21 @@ var _window = $(window),
     _header = $('.site-header'),
     _hamopen = $('.ham-open'),
     _logo = $('.ham-site-logo'),
+    _content = $('.ham-content'),
     heroBottom;
 
-_window.on('scroll',function(){
+_window.on('scroll', function() {
     heroBottom = $('.hero').height();
-    if(_window.scrollTop() > heroBottom){
+    if (_window.scrollTop() > heroBottom) {
         _header.addClass('transform');
         _hamopen.addClass('transform');
         _logo.addClass('transform');
-    }
-    else{
+        _content.addClass('transform');
+    } else {
         _header.removeClass('transform');
         _hamopen.removeClass('transform');
         _logo.removeClass('transform');
+        _content.removeClass('transform');
     }
 });
 
