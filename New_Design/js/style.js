b615562e83d1309b03a9dac84688d7b42d23ad18
- <nari20030928@gmail.com>
2023-01-15 09:20:20 +0900

    -

--- New_Design/js/style.js
+++ New_Design/js/style.js
@@ -1,7 +1,6 @@
 var _window = $(window),
     _header = $('.site-header'),
     _hamopen = $('.ham-open'),
-    _logo = $('.ham-site-logo'),
     _content = $('.ham-content'),
     heroBottom;
 
@@ -10,12 +9,10 @@ _window.on('scroll', function() {
     if (_window.scrollTop() > heroBottom) {
         _header.addClass('transform');
         _hamopen.addClass('transform');
-        _logo.addClass('transform');
         _content.addClass('transform');
     } else {
         _header.removeClass('transform');
         _hamopen.removeClass('transform');
-        _logo.removeClass('transform');
         _content.removeClass('transform');
     }
 });
