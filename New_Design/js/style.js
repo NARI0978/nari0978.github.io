50ec188199f1b0c469e84422c3fb1b6e5037743c
- <nari20030928@gmail.com>
2023-01-15 09:39:29 +0900

    -

--- New_Design/js/style.js
+++ New_Design/js/style.js
@@ -1,18 +1,21 @@
 var _window = $(window),
     _header = $('.site-header'),
     _hamopen = $('.ham-open'),
+    _logo = $('.ham-site-logo'),
     _content = $('.ham-content'),
     heroBottom;
 
 _window.on('scroll', function() {
-    heroBottom = $('.hero').height();
+    heroBottom = $('.sw').height();
     if (_window.scrollTop() > heroBottom) {
         _header.addClass('transform');
         _hamopen.addClass('transform');
+        _logo.addClass('transform');
         _content.addClass('transform');
     } else {
         _header.removeClass('transform');
         _hamopen.removeClass('transform');
+        _logo.removeClass('transform');
         _content.removeClass('transform');
     }
 });
