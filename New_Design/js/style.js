ac93dcbfc1117949535dd4b60355f2858516ff1d
- <nari20030928@gmail.com>
2023-04-17 20:13:20 +0900

    -

--- New_Design/js/style.js
+++ New_Design/js/style.js
@@ -1,25 +1,4 @@
-var _window = $(window),
-    _header = $('.site-header'),
-    _hamopen = $('.ham-open'),
-    _logo = $('.ham-site-logo'),
-    _content = $('.ham-content'),
-    _body = $('body'),
-    heroBottom;
-
-_window.on('scroll', function() {
-    heroBottom = $('.sw').height();
-    if (_window.scrollTop() > heroBottom) {
-        _header.addClass('transform');
-        _hamopen.addClass('transform');
-        _logo.addClass('transform');
-        _content.addClass('transform');
-    } else {
-        _header.removeClass('transform');
-        _hamopen.removeClass('transform');
-        _logo.removeClass('transform');
-        _content.removeClass('transform');
-    }
-});
+var _body = $('body');
 
 function isChecked() {
     if (document.getElementById('ham-check').checked) {
